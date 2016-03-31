583bad93d6fddacf90c3dcaa3a573367a58f6af1
Constant Meiring <constant@leap.ly>
2016-03-31 16:34:41 +0200

    flatten/encrypt PDFs

--- convert_service.rb
+++ convert_service.rb
@@ -11,6 +11,8 @@ get '/' do
 end
 
 post '/convert' do
+  pwd = `pwd`[0..-2]
+  puts 'starting conversion'
 	File.open('uploads/' + params[:datafile][:filename], "w") do |f|
 	 f.write(params[:datafile][:tempfile].read)
   end
@@ -21,8 +23,27 @@ post '/convert' do
   tmp_file_extention = File.extname( params[:datafile][:tempfile].path )
   tmp_file_basename = File.basename( params[:datafile][:tempfile].path, file_extention)
 
-  system "#{SOFFICE_PATH} --headless --convert-to pdf #{params[:datafile][:tempfile].path} --outdir converted"
-  File.rename("converted/#{tmp_file_basename}.pdf", "converted/#{file_basename}.pdf")
+  in_pdf = params[:datafile][:tempfile].path
 
+  puts 'temp path is '+in_pdf
+  # /var/folders/tm/w_dnm6b92cv2rpb3hykdxksm0000gn/T/RackMultipart20160331-50669-1l0wp4z.pdf
+  # binding.pry
+  tmp_file_name = rand(36**8).to_s(36)
+
+  if file_extention.downcase == 'pdf'
+    # If we receive a PDF, it's probably because it's encrypted or has layers. Ghostscript can decrypt it for us.
+    
+    
+    # pry
+
+    system "gs -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=#{pwd}/converted/#{tmp_file_name}.pdf #{params[:datafile][:tempfile].path}"
+    # system "gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=/Users/constantmeiring/Desktop/#{tmp_file_name}.pdf -c .setpdfwrite -f #{params[:datafile][:tempfile].path}"
+
+    in_pdf = "#{pwd}/converted/#{tmp_file_name}.pdf"
+    puts 'temp path updated to '+in_pdf
+  end
+
+  system "#{SOFFICE_PATH} --headless --convert-to pdf #{in_pdf} --outdir converted"
+  File.rename(in_pdf, "converted/#{file_basename}.pdf")
   send_file "converted/#{file_basename}.pdf", filename: "#{file_basename}.pdf"
 end
